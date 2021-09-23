e8343d5cd23ffa5695895a389f855d791b7b8b08
vmarch <marchenkoukr@gmail.com>
2021-09-23 09:31:40 +0200

    Initial Commit

---
+++ GameForFour/GameForFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameForFourApp.swift
+//  GameForFour
+//
+//  Created by Student on 23.09.21.
+//
+
+import SwiftUI
+
+@main
+struct GameForFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
